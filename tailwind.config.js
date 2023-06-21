8bc48fd012556570818542bce4a171cb790e90ae
Alexander Hill <me@alxhill.com>
2023-06-21 00:47:59 -0400

    css is hard

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        "glow": "0 0 2px rgb(1 1 1 / 0.15)",
+        "glow-md": "0 0 5px rgb(1 1 1 / 0.15)",
+        "glow-lg": "0 0 10px rgb(1 1 1 / 0.15)",
+        "glow-xl": "0 0 13px rgb(1 1 1 / 0.15)",
+        "glow-2xl": "0 0 13px rgb(1 1 1 / 0.15)"
+      }
+    },
   },
   plugins: [],
 }
