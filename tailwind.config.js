cfdf95b6805fbb076592d2c310207ba1c17d2aae
Alexander Hill <me@alxhill.com>
2023-06-21 01:33:05 -0400

    checkpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
     extend: {
       dropShadow: {
-        "glow": "0 0 2px rgb(1 1 1 / 0.15)",
-        "glow-md": "0 0 5px rgb(1 1 1 / 0.15)",
-        "glow-lg": "0 0 10px rgb(1 1 1 / 0.15)",
-        "glow-xl": "0 0 13px rgb(1 1 1 / 0.15)",
-        "glow-2xl": "0 0 13px rgb(1 1 1 / 0.15)"
+        "glow": "0 0 2px rgb(0 0 0 / 0.15)",
+        "glow-md": "0 0 5px rgb(0 0 0 / 0.15)",
+        "glow-lg": "0 0 10px rgb(0 0 0 / 0.15)",
+        "glow-xl": "0 0 13px rgb(0 0 0 / 0.15)",
+        "glow-2xl": "0 0 13px rgb(0 0 0 / 0.15)"
       }
     },
   },
